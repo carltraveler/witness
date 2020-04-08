e93279eccc7fc1de62e0db6515ac3c657450d427
steven <chenglin@ont.com>
2020-04-08 16:50:17 +0800

    update

---
+++ config_run.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+enterworkdir=$(pwd)
+witnessdir="witness"
+which git || apt install git
+echo "install git done."
+which go || apt install golang
+
+[[ -d $witnessdir ]] || git clone https://github.com/carltraveler/witness
+cd $witness; go build config_server.go
+
+./config_server
