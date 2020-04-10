996043fe9c1e600572003c9447278b90ab7c3b73
steven <chenglin@ont.com>
2020-04-10 14:53:46 +0800

    update

--- runtimeImage/newcontract.bash
+++ runtimeImage/newcontract.bash
@@ -5,9 +5,13 @@ set -ex
 dir=$(pwd)
 cd contract
 
+# save old file to restart.
+cp src/lib.rs ../lib.rs.bake
 sed -i "s/APHNPLz2u1JUXyD8rhryLaoQrW46J3P6y2/$1/g" src/lib.rs
 echo "start build rust contract."
 RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --release --target wasm32-unknown-unknown
+# restore old file.
+cp ../lib.rs.bake src/lib.rs
 echo "build rust contract success."
 which ontio-wasm-build || cargo install --git=https://github.com/ontio/ontio-wasm-build
 
