ea3556e38c6d67c1f0e3647cedbf8b87e7723694
steven <chenglin@ont.com>
2020-04-10 14:44:00 +0800

    update

--- runtimeImage/newcontract.bash
+++ runtimeImage/newcontract.bash
@@ -5,9 +5,12 @@ set -ex
 dir=$(pwd)
 cd contract
 
-sed -i "s/Ab1z3Sxy7ovn4AuScdmMh4PRMvcwCMzSNV/$1/g" src/lib.rs
+sed -i "s/APHNPLz2u1JUXyD8rhryLaoQrW46J3P6y2/$1/g" src/lib.rs
+echo "start build rust contract."
 RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --release --target wasm32-unknown-unknown
+echo "build rust contract success."
 which ontio-wasm-build || cargo install --git=https://github.com/ontio/ontio-wasm-build
 
 cd $dir
 ontio-wasm-build contract/target/wasm32-unknown-unknown/release/ogq.wasm contract.wasm
+echo "contract prepared over"
